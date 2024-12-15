a92b2ac1f7ffbc2f424e8bf5c1187f7e69bfbdb9
GragertVD <gragertvd@gmail.com>
2024-12-16 00:44:36 +0300

    feat: сделал компонент для генерации многоугольников

--- dice_for_eat/src/App.tsx
+++ dice_for_eat/src/App.tsx
@@ -3,23 +3,31 @@ import { CanvasLayout } from './canvas/CanvasLayout'
 import { Dice } from './canvas/Dice'
 import { Light } from './canvas/Light'
 import { TableBox } from './canvas/TableBox'
-import { OrbitControls } from '@react-three/drei'
-import { DiceEdge } from './canvas/DiceEdge'
+import { OrbitControls, Text } from '@react-three/drei'
+import { Polygon } from './canvas/Polygon'
+import { DoubleSide } from 'three'
+import * as THREE from "three";
 
 function App() {
 
-  return (
-    <CanvasLayout>
-      {/* <PointerLockControls /> */}
-      <OrbitControls />
-      <Physics gravity={[0, -10, 0]}>
-        <Dice />
-        <TableBox />
-        <DiceEdge />
-        <Light />
-      </Physics>
-    </CanvasLayout>
-  )
+	return (
+		<CanvasLayout>
+			{/* <PointerLockControls /> */}
+			<OrbitControls />
+			<Physics gravity={[0, -10, 0]}>
+				<Dice />
+				<TableBox />
+				<Polygon countEdge={6} position={[0, 2, -3]} rotation={[0, 0, 0]} material={{ color: new THREE.Color(0xffffff), side: DoubleSide }}>
+					<mesh rotation={[Math.PI / 2, Math.PI, 0]} position={[0, 0.001, 0]}>
+						<Text color={'red'} fontSize={0.4}>
+							Test
+						</Text>
+					</mesh>
+				</Polygon>
+				<Light />
+			</Physics>
+		</CanvasLayout>
+	)
 }
 
 export default App
